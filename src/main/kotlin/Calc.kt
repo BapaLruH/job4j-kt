b896535046db5604ea27cd397f3cce597d889267
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2020-03-08 17:58:48 +0300

    2. Типы данных и методы. [#188826]

---
+++ src/main/kotlin/Calc.kt
@@ -0,0 +1,15 @@
+fun add(first : Int, second : Int) : Int {
+    return first + second
+}
+
+fun subtract(first: Int, second: Int) : Int {
+    return first - second
+}
+
+fun multiple(first: Int, second: Int) : Int {
+    return first * second
+}
+
+fun divide(first: Int, second: Int) : Int {
+    return first / second
+}
--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello from kotlin")
+    val age = 18
+    val last = "Ivanov"
+    val name = "Ivan"
+    val size = 65
+
+    println("Age : $age, last : $last, name : $name, size : $size")
+    println("result : ${add(1, 2)}")
+    println("result : ${subtract(1, 2)}")
+    println("result : ${multiple(1, 2)}")
+    println("result : ${divide(1, 2)}")
 }
