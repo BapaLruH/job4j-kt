9f7e552a5e0a512fcfa3e4bc700e807a0e1394a8
Evgeny Sh <xxx-zet-xxx@yandex.ru>
2020-03-08 20:50:48 +0300

    5. Циклы while и for [#189222]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,58 @@ fun max(first: Int, second: Int) = if (first > second) first else second
 
 fun max(first: Int, second: Int, third: Int) = if (max(first, second) > third) max(first, second) else third
 
+fun fitness(ivan: Int, nik: Int): Int {
+    var month = 0
+    var powerIvan = ivan
+    var powerNikolay = nik
+    while (powerIvan < powerNikolay) {
+        powerIvan *= 3
+        powerNikolay *= 2
+        month++
+    }
+    return month
+}
+
+fun sum(start: Int, finish: Int): Int {
+    var rsl = 0
+    for (value in start..finish) {
+        rsl += value
+    }
+    return rsl
+}
+
+fun sumUntilFinish(start: Int, finish: Int): Int {
+    var rsl = 0
+    for (value in start until finish) {
+        rsl += value
+    }
+    return rsl
+}
+
+fun sumByStep2(start: Int, finish: Int): Int {
+    var rsl = 0
+    for (value in start..finish step 2) {
+        rsl += value
+    }
+    return rsl
+}
+
+fun sumDownTo(start: Int, finish: Int): Int {
+    var rsl = 0
+    for (value in finish downTo start step 2) {
+        rsl += value
+    }
+    return rsl
+}
+
+fun draw(size: Int) {
+    if (size > 0 && size % 2 != 0) {
+        for (value in 0..size step 2) {
+            print("X")
+        }
+    }
+}
+
 fun main() {
     val age = 18
     val last = "Ivanov"
@@ -15,4 +67,10 @@ fun main() {
     println("result : ${divide(1, 2)}")
     println("max from 1 and 2 is ${max(1, 2)}")
     println("max from 1 and 2 and 3 is ${max(1, 3, 2)}")
+    println("Month : ${fitness(50, 90)}")
+    println("Sum : ${sum(1, 5)}")
+    println("Sum : ${sumUntilFinish(1, 5)}")
+    println("Sum : ${sumByStep2(1, 5)}")
+    println("Sum : ${sumDownTo(1, 5)}")
+    draw(51)
 }
